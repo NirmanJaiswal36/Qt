d00766cde2a6b2d5066435ff293d1b32ccd44532
NirmanJaiswal36 <nirmanjaiswal0369@gmail.com>
2024-05-09 00:36:28 +0530

    sample usage of osm

--- osm.py
+++ osm.py
@@ -1,4 +1,37 @@
+import sys
 import osmnx as ox
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 
-p = ox.geocode_to_gdf("Gujarat,India")
-p.plot()
+class MapWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("OSMnx Map Viewer")
+        self.setGeometry(100, 100, 800, 600)
+        
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
+        
+        self.plot_map()
+
+    def plot_map(self):
+        # Retrieve geographic data using OSMnx
+        gdf = ox.geocode_to_gdf("Gujarat, India")
+        
+        # Create a figure and axes for the plot
+        fig, ax = plt.subplots()
+        
+        # Plot the geographic data
+        gdf.plot(ax=ax)
+        
+        # Embed the plot in a PyQt5 canvas
+        canvas = FigureCanvas(fig)
+        self.layout.addWidget(canvas)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MapWindow()
+    window.show()
+    sys.exit(app.exec_())
