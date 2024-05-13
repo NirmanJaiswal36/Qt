9a277390ca4347564c65b06e06e6b61dc1929d36
NirmanJaiswal36 <nirmanjaiswal0369@gmail.com>
2024-05-13 16:52:28 +0530

    added QlineEdit

--- basics.py
+++ basics.py
@@ -1,6 +1,6 @@
 from turtle import onclick
 from PyQt5.QtCore import Qt,pyqtSlot
-from PyQt5.QtWidgets import QApplication,QMainWindow,QWidget,QPushButton
+from PyQt5.QtWidgets import QApplication,QMainWindow,QWidget,QPushButton, QLineEdit, QMessageBox, QAction
 from PyQt5.QtGui import QIcon
 import sys
 
@@ -17,15 +17,24 @@ class App(QMainWindow):
 
         self.setWindowTitle(self.title)
         self.setGeometry(100,100,300,300)
-        self.button = QPushButton("Import Shapefile",self)
-        self.button.move(10,20)
+
+        self.textbox = QLineEdit(self)
+        self.textbox.move(5,20)
+        self.textbox.resize(300,100)
+
+        self.button = QPushButton("Show Text",self)
+        self.button.move(100,150)
+        self.button.setToolTip("shows the text written in a message box")
         self.button.clicked.connect(self.onclick)
         self.show()
     
     @pyqtSlot()
 
     def onclick(self):
-        print("Shapefile imported")
+        text = self.textbox.text()
+        QMessageBox.question(self,"Message","You typed: \n"+text,QMessageBox.Ok,QMessageBox.Ok)
+        self.textbox.setText("")
+
         
 if __name__== '__main__':
     app = QApplication(sys.argv)
