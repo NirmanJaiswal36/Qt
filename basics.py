0aa40de828e29ab5aa6a080d8962dc1a622a5bd8
NirmanJaiswal36 <nirmanjaiswal0369@gmail.com>
2024-05-13 14:56:02 +0530

    added pushbutton

--- basics.py
+++ basics.py
@@ -1,5 +1,6 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication,QMainWindow,QWidget
+from turtle import onclick
+from PyQt5.QtCore import Qt,pyqtSlot
+from PyQt5.QtWidgets import QApplication,QMainWindow,QWidget,QPushButton
 from PyQt5.QtGui import QIcon
 import sys
 
@@ -9,11 +10,22 @@ class App(QMainWindow):
         self.title = "Main Window"
         self.initUI()
     
+    
+
+    
     def initUI(self):
 
         self.setWindowTitle(self.title)
         self.setGeometry(100,100,300,300)
+        self.button = QPushButton("Import Shapefile",self)
+        self.button.move(10,20)
+        self.button.clicked.connect(self.onclick)
         self.show()
+    
+    @pyqtSlot()
+
+    def onclick(self):
+        print("Shapefile imported")
         
 if __name__== '__main__':
     app = QApplication(sys.argv)
